27bac56d447ce157a19cde850d6c80993559a88b
Antwan van Houdt <jabwd@exurion.com>
2013-08-02 20:11:18 +0200

    Properly updated the version number too

--- Classes/NKWebsite.php
+++ Classes/NKWebsite.php
@@ -23,7 +23,7 @@ require_once 'NetKit/Classes/Views/default/NKMainView.php';
 
 class NKWebsite
 {
-	const NetKitVersion = "0.16.1";
+	const NetKitVersion = "0.16.2";
 	
 	private static $_sharedInstance;
 	private $_controller;
