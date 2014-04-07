e00a74b6096bb395471417784f134e6aba062e29
Antwan van Houdt <jabwd@exurion.com>
2014-04-07 11:03:00 +0200

    New release version of netkit 0.22

--- Classes/NKWebsite.php
+++ Classes/NKWebsite.php
@@ -22,7 +22,7 @@ require_once 'NetKit/Classes/Views/default/NKMainView.php';
 
 class NKWebsite
 {
-	const NetKitVersion = "0.21.1";
+	const NetKitVersion = "0.22.0";
 	
 	private static $_sharedInstance;
 	private $_controller;
